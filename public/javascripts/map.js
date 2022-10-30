0a9e5ca29b737af5dbba6e741bbb0264aee4dffe
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2022-10-31 03:30:54 +0530

    Updated

---
+++ public/javascripts/map.js
@@ -0,0 +1,13 @@
+console.log("map.js loaded");
+
+$('.pin').on("click", function() {
+	if ( $(this).hasClass('is-opened') ) {
+		$(this).toggleClass('is-opened');
+		return;
+	}
+	
+	$('.pin').each(function() {
+		$(this).removeClass('is-opened');
+	});
+	$(this).toggleClass('is-opened');
+})
