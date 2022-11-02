5c99448aefd606544077887924fb8bdaa2e3e25a
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2022-11-02 21:51:20 +0530

    updated

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,5 +1,6 @@
 console.log("map.js loaded");
 
+
 $('.pin').on("click", function() {
 	if ( $(this).hasClass('is-opened') ) {
 		$(this).toggleClass('is-opened');
@@ -11,3 +12,21 @@ $('.pin').on("click", function() {
 	});
 	$(this).toggleClass('is-opened');
 })
+
+function load2(selectedvalue) {
+	console.log(selectedvalue);
+	
+
+}
+
+function load(selectedvalue,username) {
+	console.log(selectedvalue);
+	fetch("http://localhost:9000/cs", { 
+		  method: 'POST',
+		  headers: {'Content-Type': 'application/json'},
+		  body: JSON.stringify({"selectedvalue": selectedvalue,"username":username})
+	  });
+  }
+
+
+
